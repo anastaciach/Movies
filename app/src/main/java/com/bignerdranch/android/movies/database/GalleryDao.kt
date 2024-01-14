76ae03882abf259d4a587cf328024ef3e9e7582a
Anastasia <aachuchkalova@gmail.com>
2024-01-14 23:32:20 +0700

    Добавление функции deletemovie для удаления записи из таблицы gallery

---
+++ app/src/main/java/com/bignerdranch/android/movies/database/GalleryDao.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.movies.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.bignerdranch.android.movies.Item
+
+@Dao
+
+interface GalleryDao {
+    @Query("SELECT * FROM gallery")
+    fun getmovies(): LiveData<List<Item>>
+    @Insert
+    fun addmovie(item: Item)
+
+    @Query("DELETE FROM gallery WHERE id=(:id)")
+    fun deletemovie(id: String)
+
+    @Query("SELECT * FROM gallery WHERE id=(:id)")
+    fun getmovie(id: String): Item?
+}
