e2e237a6d361a53f070ad91ff3d60754f12dde7c
Anastasia <aachuchkalova@gmail.com>
2024-01-15 21:39:27 +0700

    Изменены функции для получения и сохранения запроса и года в настройках приложения

--- app/src/main/java/com/bignerdranch/android/movies/QueryPreferences.kt
+++ app/src/main/java/com/bignerdranch/android/movies/QueryPreferences.kt
@@ -6,19 +6,19 @@ import androidx.core.content.edit
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
 
+private const val PREF_SEARCH_QUERY_YEAR = "searchQueryYear"
 object QueryPreferences {
-    fun getStoredQuery(context: Context):
-            String {
-        val prefs =
-            PreferenceManager.getDefaultSharedPreferences(context)
-        return prefs.getString(PREF_SEARCH_QUERY, "")!!
+    fun getStoredQueryByYear(context: Context): Pair<String, String> {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val query = prefs.getString(PREF_SEARCH_QUERY, "")!!
+        val year = prefs.getString(PREF_SEARCH_QUERY_YEAR, "")!!
+        return Pair(query, year)
     }
-    fun setStoredQuery(context: Context, query: String) {
+    fun setStoredQueryByYear(context: Context, query: String, year:String) {
         PreferenceManager.getDefaultSharedPreferences(context)
             .edit()
-            .putString(PREF_SEARCH_QUERY,query)
+            .putString(PREF_SEARCH_QUERY, query)
+            .putString(PREF_SEARCH_QUERY_YEAR, year)
             .apply()
-    }
-
     }
 }
